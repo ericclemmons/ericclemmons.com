d996cac3403c7a581afc609898ccf9ce94fdef08
Eric Clemmons <eric@smarterspam.com>
2019-07-13 16:51:56 -0500

    Export /components

---
+++ components/index.ts
@@ -0,0 +1,37 @@
+import a from "./a";
+import blockquote from "./blockquote";
+import Card from "./Card";
+import h1 from "./h1";
+import h2 from "./h2";
+import h3 from "./h3";
+import h4 from "./h4";
+import hr from "./hr";
+import img from "./img";
+import inlineCode from "./inlineCode";
+import Layout from "./Layout";
+import li from "./li";
+import ol from "./ol";
+import p from "./p";
+import pre from "./pre";
+import strong from "./strong";
+import ul from "./ul";
+
+export {
+  a,
+  blockquote,
+  Card,
+  h1,
+  h2,
+  h3,
+  h4,
+  hr,
+  img,
+  inlineCode,
+  li,
+  Layout,
+  ol,
+  p,
+  pre,
+  strong,
+  ul
+};
